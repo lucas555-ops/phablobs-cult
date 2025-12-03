e65eefe5e543b1d882f71de52e1228211393eaec
lucas555-ops <lucas9589716@gmail.com>
2025-12-03 18:58:26 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.phantom.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.phantom.app',
+      },
+    ],
+    unoptimized: true, // SVG не требует оптимизации
   },
   headers: async () => [
     {
