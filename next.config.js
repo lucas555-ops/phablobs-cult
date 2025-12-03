4581622b80dae24c2a42ccc4b8c4fe7cdc08530b
lucas555-ops <lucas9589716@gmail.com>
2025-12-03 14:52:27 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,30 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+      ],
+    },
+  ],
+}
+
+module.exports = nextConfig
+```
