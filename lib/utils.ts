8c02195717bb8d548d8fdc459561b464457f9a2c
lucas555-ops <lucas9589716@gmail.com>
2025-12-04 11:04:44 +0500

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function cn(...classes: (string | boolean | undefined)[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export function truncateAddress(address: string, startChars = 6, endChars = 4): string {
-  if (address.length <= startChars + endChars) return address
-  return `${address.substring(0, startChars)}...${address.substring(address.length - endChars)}`
-}
