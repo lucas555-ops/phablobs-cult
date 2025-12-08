6583e39f0a3ebffd703f33616eae51aabb09c028
lucas555-ops <lucas9589716@gmail.com>
2025-12-08 13:41:15 +0500

    Update route.ts

--- app/api/phablob/[address]/route.ts
+++ app/api/phablob/[address]/route.ts
@@ -2,7 +2,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import fs from 'fs'
 import path from 'path'
-import { Resvg } from '@resvg/resvg-js'
+import { Resvg, initWasm } from '@resvg/resvg-wasm'
+import resvgWasmUrl from '@resvg/resvg-wasm/index.wasm'
 
 // Ваша цветовая система (оставлена как есть)
 import { 
@@ -10,12 +11,19 @@ import {
   generateSolidBgFromBalance
 } from '@/lib/color-tiers'
 
-// Шрифт DejaVu Sans Bold в Base64 (только латиница, чтобы не раздувать размер)
-// Это небольшой фрагмент шрифта, достаточно для PHANTOM/PHABLOBS и цифр
-const DEJAVU_SANS_BOLD_BASE64 = 'd09GRgABAAAAAGhYABIAAAAAyLwAAQABAAAAAAAAAAAAAAAAAAAAAAAAAABHREVGAAABbAAAAC4AAAA0AsQC9UdQT1MAAAGcAAABIgAABpxbKRa1R1NVQgAAAfwAAAQEAAAINq4Xx09TLzIAAAIoAAAAYAAAAGAIIvzVY21hcAAAAmgAAAFUAAABwqkIPuljdnQgAAADtAAAAAYAAAAGAScB8GZwZ20AAAPMAAABsQAAAmVTtC+nZ2FzcAAABQwAAAAIAAAACAAAABBnbHlmAAAFGAAAWU4AAK1AhllGRGhlYWQAAGkMAAAAMwAAADYgOYrgaGhlYQAAaTgAAAAhAAAAJA8HCPJobXR4AABpYAAAAiwAAAJ0HBAJfGxvY2EAAGqYAAABLAAAASx5DpmwbWF4cAAAa4gAAAAgAAAAIAIZB45uYW1lAABrqAAAAXEAAAKyBkkcJXBvc3QAAG4MAAAB9wAAAwCAAK0IcHJlcAAAb6wAAABAAAAAQHlQ1V4AAAABAAAAANBkZHQAAAAIAAAAARAt0IzeJxjYGRgYOBiMGCwY2BycfMJYRLz2fPZZzYJINyQxMDGMAYAK80D4XicY2BkYWCcwMDKwMHUyXSGgYGhH0IzPmAwZGRiYGVmYGLGAwUzwvMgkwHMDgwM7wHMDgzMCgxMeVyMDAcYGEBAFQ0MqngcGBQeMDP8/w8UZGBgYJRhYAYAjvgJcQB4nK1Za3Bc1Xn+vnN3V7tarVZarVZ6Wm89rAesB2xjg2yMjG3ANhj84IExYMy/jg02wWEbDAESQhJc0qQkHZpm8D9oaNo/SadM02k6nXTSadpO+WGZJqSTTtM2TSeN/4Xve87VlQzKDM24Z++9Z8/5zne+833Pc85ZiTj++V98CF5kLpQsUy2zp9vyWI4Jtgl2xYI5NimOufa5jnmuBZ7ARL91vv3TiC9X5Jhf6P9KwdzZ82bO/8qs2fNmyvOLZ33F7WcsKpjLF4UF9hIR+92/gfy3f+e1+OG+gd2/jv/6+BNPivjTj42r8X/7dbyp9lQ8/vT+u+JP7t8ff1IdH42Pxp9Sbv7uv/0b9vJPsK/ZtCn+5MjDkMLxZ37wq/jT3314ZGRE/eEzz4nYd3/10+8+8lT8ie9+65GHH378+w8//uP40z/4+q7auXNE7PvxmKpKf/3//4m8Jf4U25oDkLj9n8fPnvs3uB7/xlPC+9bR+N9dGhXhJzrEkc9K4tGvvI5z4q/ih+NuvI7vv3Em/vKj8TPs5fzX/o7/5M9++v9bvgI+1qJMuY7f43sM9gvg9JgKqwD8aVAwU0DBNN8KP5rl2+k/PF7oP+//Cv3XQwU0AZ3gKVRBE9FEn7AbTUTmM6d/AZwQAhQIfq4Bm4CdoZ0L5nq0Hqt1Pbof4R3A/D6U76Zx+38IYRNiF3xIhMkvft28LzH/S/wL/u5P5g0f0nvy0a2P/Vj8w9nP4vt3X4ovn9eP3z73cvxP59Tg6pV9+MPVNbz9wlv4lwcu4v3HbuL9p99O4G+fiuA//uYdXH14HQ+fvIxf3LwR7z9zLX7zD7c0/PTVG/G/3PRo/I9P3x0/e+jO+MUL/fHzY4PxMydG8fTp3fHjJ3fFj57YhSOj2/HVz/fGn/3S2vj+L7TEB3vq4vs2N8f39jTEB9bVxXesqY3vrK6Jb6xYFn+yoj7etbI+3tHcGO9ovz3e2dYab1/VGl/b3h7vWt0Z71m3Nt69fkO8Z1NPvHfz5njf1q3xgYGBeN+2bfH+7dvj/Tt2xAd27owP7toV37V7d3xwz5744J498cHh4fjQvn1Zz/3iT6/Ht23bxq4d6FcUf+jUKXHr5k1x//nz4vo718SFc+fEoX1fFKr6iNA0FV9qHxNvH3ld/Pe9f3n72w1/fvu9oUviA/GBeE88ElfFG+L+uCIexk3xhFDELaGKP4o3hcLeS+KSuA/7xCPxADyFvwu4Lr6OPeIR8YD4pnhdDItj4oi4Jh4Uv8b6u+JZ0ST+Ydxc9beyqVn0sS/SxnJpF4X+v4j/gs0XwOSkeCJ+KH4k/t24w/69eEW8Ij4Q58Xb4l/kPvEG/Ge0/zlgc2BcXG19nd0C2gH2HfCMfA/V4Dg2gzYjNg8yF2jLsWPYqNh6oFzOGXOxH8p3+U3zfoB/QB87mAz8/xfYR1Q8Q3gvOMJXIBI8oEQugPu0PsRzD/2k7Wn90f3uGv5d+BUo6GfWt+b/PnzL9nJty6cpHy/LHH7I39f6/UU7u+gzf6P/2P29lv/Szq39/lf8z/yvpZ5/w7Z/qN6F/j8F/Xfwf6L+fy1Bk92x2cGHO5BBfDsklJ/ouhAq8K8H9gOjgPrzV7l+9tA18ddjM8Ufn/hg4jf3XBWPLP1jcX/0EfHQfNX45fS7jG9+8oZ49/a34//92QfxL0/5//HjX2rG6Bf9jLxknAwO6UQKDBnIV5n7s9N2fwn/fxl9+gnlq4/m/w8VfTfiw7Y+UO5b6k8Z9dv7/6HyR3Jvo9/v9B8m+ZP10PQv9e/D7Sf6K4N3v5/s2/NP/tjzr6P9T8Rc+Ocj8V/hg1x6aB2n3/xI/Jc+f1T+t30++Vf/Kz6/0j+5/av0Hz5D8d/Z/lP9d9pQy/8I2/vX1/s/6P85/gvlP8d/WHb/S+l1e/+dfOXD3U5/XlJkzQAv8DJWboANmAT3aeMF+i6zzQq7Bf3t/jMQ4R8zHkqfaUdoC2gzpH5/3Gc5aQ/U3+of+8g+f2Px/yT9v9QHH1H/p7JvZ38yr0j/cY/kPz/+n/3/v+7zn9qQlU7b67Hs+c/qw8fH6B9t/7vXaNl6KXZ3PrDrf2b8n9u/Qp+C2WJvG/4P0zN5HhT0z3T9p9fnv+2H/dqNc9A5yN8Zuk5/sJv+8a/vD3/H6qP7T9t/1xpnv/+V/F8pv57S/1Pk/1P5bWJ6Lb8O7tb2/O97/rN9k/P/R9p3kfDf+X+W/6wzqTsdH/Qx/b+n5yq0vn6P9iOsA/17hNbph9w9X5B1ps7fgZ4kz9O8fy5qHpz1Q+7PuDfeZ36e1f+n9g+5Ef/7jufv8D/9B7+3V/9/7P+P1v+rP4Z9V3q+lTb55kz5v+O+r/F/7iH5H4n/3Pn1d8D/H8Y/1fy1Hzl/B/5/tP9D/S8b/QX//9P9r0x5/+/9Uf3zn73/s/9n6fVfwT6X/9z9T0wOfGDn/l9BH3pn/4CJ/Teg5ya0Pjiw/2GvA9P7//fr/+37T8Vv5f/7/4+//8/5/9PE/xPsf6/vC/8/5JcQ/7H3f3b9n9T/pP7/bPYf2v8P3K9A33GXo+vt2v+H68sQP7+16v0c+1/6/0fMv/2+1kcnfw/l8v/G/Z9T/x8m/x/IfzW7Xzb+W1s/oM9v/6jf/9n9Q9o//P/O9X9a/i3/n6Yf7fkz+C9/f5yrQ3L9T8//f8v9/wr8N9w3pP0v7bPA/Nq3Y3/2/pQnL+HfzP0LPJY/1l+hvG6g/bP3vv7z9v+S2cLP+/2d8//j9P8e1/F/dvx/iJ8f+//t77//6fP/P/z6P9D7E/Za7fYf4/NP/jr9f9L3//8D6/t8/5f5r/q6/g8lvrN+n/CjA+wE+yY0lgrwCLyR8z/s94dSf3z2/5/9f2j+y3x/tH3/p8+f/b7Z/q9I/+fXg/4z23/N+xj9/99n/+7Z/C/xv7XJdb7/g7WQf5/8/4p8X5PYP5l3eND1ccXm3x9J7o/a+u9/+pL0f6r/Y/F/uP4H7n/S7E/OP8jff/3p/eefPwz+i9q38/tv5L5+P8L+/QH8n7B/kf/fv/7/2/KPkf+7/f/t8l/ut12Hftn/VpW5fqG3Qf/+Uc1/avj9jzn7/+Pj+vPv/5f2n8/99y/w/0v/fw76P3f/FxXJ+28fMf/wb3sf4f8Fv/L9g2r/9mnH97/0/P9vwf+h9n/O/B9+/g1/Z+U/aZ9Svh/lv/j/+v/39X9u6ac3/3P/f/7/S/Mv+K+z/Hz//6v8n1/r+0/m3H3//4b/z++/f4H7n9z8n0P+P6T+7w/63w+P5P/b+P/j+Z/Lf/1G/2PN9p/t37Qz5j3l/9t/9n9MefJf8P4d/8v6vNvtv+Nl/U/0f/vT/9/W/4fz/8f8n+32f938p7/C/6Tz/jC/y99xO93yZ+N0g/U+nn75P9N+B/8/H3/5a//3L+wX/4B/9f3H8BxK7PLW/r3T+/Wb/AA=='
-
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
+
+// Инициализируем WASM один раз
+let wasmInitialized = false
+
+async function initializeResvg() {
+  if (!wasmInitialized) {
+    const wasmBuffer = await fetch(resvgWasmUrl).then(res => res.arrayBuffer())
+    await initWasm(wasmBuffer)
+    wasmInitialized = true
+  }
+}
 
 function isValidSolanaAddress(address: string): boolean {
   return /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(address)
@@ -92,6 +100,9 @@ async function generateCompleteSVG(publicKey: string): Promise<string> {
 
   console.log(`🎨 Generating Phablob #${phablobNumber}`)
 
+  // Шрифт DejaVu Sans Bold в Base64
+  const DEJAVU_SANS_BOLD_BASE64 = 'd09GRgABAAAAAGhYABIAAAAAyLwAAQABAAAAAAAAAAAAAAAAAAAAAAAAAABHREVGAAABbAAAAC4AAAA0AsQC9UdQT1MAAAGcAAABIgAABpxbKRa1R1NVQgAAAfwAAAQEAAAINq4Xx09TLzIAAAIoAAAAYAAAAGAIIvzVY21hcAAAAmgAAAFUAAABwqkIPuljdnQgAAADtAAAAAYAAAAGAScB8GZwZ20AAAPMAAABsQAAAmVTtC+nZ2FzcAAABQwAAAAIAAAACAAAABBnbHlmAAAFGAAAWU4AAK1AhllGRGhlYWQAAGkMAAAAMwAAADYgOYrgaGhlYQAAaTgAAAAhAAAAJA8HCPJobXR4AABpYAAAAiwAAAJ0HBAJfGxvY2EAAGqYAAABLAAAASx5DpmwbWF4cAAAa4gAAAAgAAAAIAIZB45uYW1lAABrqAAAAXEAAAKyBkkcJXBvc3QAAG4MAAAB9wAAAwCAAK0IcHJlcAAAb6wAAABAAAAAQHlQ1V4AAAABAAAAANBkZHQAAAAIAAAAARAt0IzeJxjYGRgYOBiMGCwY2BycfMJYRLz2fPZZzYJINyQxMDGMAYAK80D4XicY2BkYWCcwMDKwMHUyXSGgYGhH0IzPmAwZGRiYGVmYGLGAwUzwvMgkwHMDgwM7wHMDgzMCgxMeVyMDAcYGEBAFQ0MqngcGBQeMDP8/w8UZGBgYJRhYAYAjvgJcQB4nK1Za3Bc1Xn+vnN3V7tarVZ6Wm89rAesB2xjg2yMjG3ANhj84IExYMy/jg02wWEbDAESQhJc0qQkHZpm8D9oaNo/SadM02k6nXTSadpO+WGZJqSTTtM2TSeN/4Xve87VlQzKDM24Z++9Z8/5zne+893Pc85ZiTj++V98CF5kLpQsUy2zp9vyWI4Jtgl2xYI5NimOufa5jnmuBZ7ARL91vv3TiC9X5Jhf6P9KwdzZ82bO/8qs2fNmyvOLZ33F7WcsKpjLF4UF9hIR+92/gfy3f+e1+OG+gd2/jv/6+BNPivjTj42r8X/7dbyp9lQ8/vT+u+JP7t8ff1IdH42Pxp9Sbv7uv/0b9vJPsK/ZtCn+5MjDkMLxZ37wq/jT3314ZGRE/eEzz4nYd3/10+8+8lT8ie9+65GHH378+w8//uP40z/4+q7auXNE7PvxmKpKf/3//4m8Jf4U25oDkLj9n8fPnvs3uB7/xlPC+9bR+N9dGhXhJzrEkc9K4tGvvI5z4q/ih+NuvI7vv3Em/vKj8TPs5fzX/o7/5M9++v9bvgI+1qJMuY7f43sM9gvg9JgKqwD8aVAwU0DBNN8KP5rl2+k/PF7oP+//Cv3XQwU0AZ3gKVRBE9FEn7AbTUTmM6d/AZwQAhQIfq4Bm4CdoZ0L5nq0Hqt1Pbof4R3A/D6U76Zx+38IYRNiF3xIhMkvft28LzH/S/wL/u5P5g0f0nvy0a2P/Vj8w9nP4vt3X4ovn9eP3z73cvxP59Tg6pV9+MPVNbz9wlv4lwcu4v3HbuL9p99O4G+fiuA//uYdXH14HQ+fvIxf3LwR7z9zLX7zD7c0/PTVG/G/3PRo/I9P3x0/e+jO+MUL/fHzY4PxMydG8fTp3fHjJ3fFj57YhSOj2/HVz/fGn/3S2vj+L7TEB3vq4vs2N8f39jTEB9bVxXesqY3vrK6Jb6xYFn+yoj7etbI+3tHcGO9ovz3e2dYab1/VGl/b3h7vWt0Z71m3Nt69fkO8Z1NPvHfz5njf1q3xgYGBeN+2bfH+7dvj/Tt2xAd27owP7toV37V7d3xwz5744J498cHh4fjQvn1Zz/3iT6/Ht23bxq4d6FcUf+jUKXHr5k1x//nz4vo718SFc+fEoX1fFKr6iNA0FV9qHxNvH3ld/Pe9f3n72w1/fvu9oUviA/GBeE88ElfFG+L+uCIexk3xhFDELaGKP4o3hcLeS+KSuA/7xCPxADyFvwu4Lr6OPeIR8YD4pnhdDItj4oi4Jh4Uv8b6u+JZ0ST+Ydxc9beyqVn0sS/SxnJpF4X+v4j/gs0XwOSkeCJ+KH4k/t24w/69eEW8Ij4Q58Xb4l/kPvEG/Ge0/zlgc2BcXG19nd0C2gH2HfCMfA/V4Dg2gzYjNg8yF2jLsWPYqNh6oFzOGXOxH8p3+U3zfoB/QB87mAz8/xfYR1Q8Q3gvOMJXIBI8oEQugPu0PsRzD/2k7Wn90f3uGv5d+BUo6GfWt+b/PnzL9nJty6cpHy/LHH7I39f6/UU7u+gzf6P/2P29lv/Szq39/lf8z/yvpZ5/w7Z/qN6F/j8F/Xfwf6L+fy1Bk92x2cGHO5BBfDsklJ/ouhAq8K8H9gOjgPrzV7l+9tA18ddjM8Ufn/hg4jf3XBWPLP1jcX/0EfHQfNX45fS7jG9+8oZ49/a34//92QfxL0/5//HjX2rG6Bf9jLxknAwO6UQKDBnIV5n7s9N2fwl/fxl9+gnlq4/m/w8VfTfiw7Y+UO5b6k8Z9dv7/6HyR3Jvo9/v9B8m+ZP10PQv9e/D7Sf6K4N3v5/s2/NP/tjzr6P9T8Rc+Ocj8V/hg1x6aB2n3/xI/Jc+f1T+t30++Vf/Kz6/0j+5/av0Hz5D8d/Z/lP9d9pQy/8I2/vX1/s/6P85/gvlP8d/WHb/S+l1e/+dfOXD3U5/XlJkzQAv8DJWboANmAT3aeMF+i6zzQq7Bf3t/jMQ4R8zHkqfaUdoC2gzpH5/3Gc5aQ/U3+of+8g+f2Px/yT9v9QHH1H/p7JvZ38yr0j/cY/kPz/+n/3/v+7zn9qQlU7b67Hs+c/qw8fH6B9t/7vXaNl6KXZ3PrDrf2b8n9u/Qp+C2WJvG/4P0zN5HhT0z3T9p9fnv+2H/dqNc9A5yN8Zuk5/sJv+8a/vD3/H6qP7T9t/1xpnv/+V/F8pv57S/1Pk/1P5bWJ6Lb8O7tb2/O97/rN9k/P/R9p3kfDf+X+W/6wzqTsdH/Qx/b+n5yq0vn6P9iOsA/17hNbph9w9X5B1ps7fgZ4kz9O8fy5qHpz1Q+7PuDfeZ36e1f+n9g+5Ef/7jufv8D/9B7+3V/9/7P+P1v+rP4Z9V3q+lTb55kz5v+O+r/F/7iH5H4n/3Pn1d8D/H8Y/1fy1Hzl/B/5/tP9D/S8b/QX//9P9r0x5/+/9Uf3zn73/s/9n6fVfwT6X/9z9T0wOfGDn/l9BH3pn/4CJ/Teg5ya0Pjiw/2GvA9P7//fr/+37T8Vv5f/7/4+//8/5/9PE/xPsf6/vC/8/5JcQ/7H3f3b9n9T/pP7/bPYf2v8P3K9A33GXo+vt2v+H68sQP7+16v0c+1/6/0fMv/2+1kcnfw/l8v/G/Z9T/x8m/x/IfzW7Xzb+W1s/oM9v/6jf/9n9Q9o//P/O9X9a/i3/n6Yf7fkz+C9/f5yrQ3L9T8//f8v9/wr8N9w3pP0v7bPA/Nq3Y3/2/pQnL+HfzP0LPJY/1l+hvG6g/bP3vv7z9v+S2cLP+/2d8//j9P8e1/F/dvx/iJ8f+//t77//6fP/P/z6P9D7E/Za7fYf4/NP/jr9f9L3//8D6/t8/5f5r/q6/g8lvrN+n/CjA+wE+yY0lgrwCLyR8z/s94dSf3z2/5/9f2j+y3x/tH3/p8+f/b7Z/q9I/+fXg/4z23/N+xj9/99n/+7Z/C/xv7XJdb7/g7WQf5/8/4p8X5PYP5l3eND1ccXm3x9J7o/a+u9/+pL0f6r/Y/F/uP4H7n/S7E/OP8jff/3p/eefPwz+i9q38/tv5L5+P8L+/QH8n7B/kf/fv/7/2/KPkf+7/f/t8l/ut12Hftn/VpW5fqG3Qf/+Uc1/avj9jzn7/+Pj+vPv/5f2n8/99y/w/0v/fw76P3f/FxXJ+28fMf/wb3sf4f8Fv/L9g2r/9mnH97/0/P9vwf+h9n/O/B9+/g1/Z+U/aZ9Svh/lv/j/+v/39X9u6ac3/3P/f/7/S/Mv+K+z/Hz//6v8n1/r+0/m3H3//4b/z++/f4H7n9z8n0P+P6T+7w/63w+P5P/b+P/j+Z/Lf/1G/2PN9p/t37Qz5j3l/9t/9n9MefJf8P4d/8v6vNvtv+Nl/U/0f/vT/9/W/4fz/8f8n+32f938p7/C/6Tz/jC/y99xO93yZ+N0g/U+nn75P9N+B/8/H3/5a//3L+wX/4B/9f3H8BxK7PLW/r3T+/Wb/AA=='
+
   return `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="800" height="800" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
   <defs>
@@ -123,7 +134,7 @@ async function generateCompleteSVG(publicKey: string): Promise<string> {
   <!-- ФОН -->
   <rect width="800" height="800" fill="${bgColor2 ? 'url(#bgGrad)' : bgColor}"/>
   
-  <!-- ВОДЯНЫЕ ЗНАКИ (используем один элемент text с tspans для оптимизации) -->
+  <!-- ВОДЯНЫЕ ЗНАКИ -->
   <text font-family="DejaVu Sans, Roboto, sans-serif" font-weight="900" fill="white" opacity="0.07">
     <tspan x="100" y="150" font-size="48" transform="rotate(-15 100 150)">PHANTOM</tspan>
     <tspan x="600" y="200" font-size="42" transform="rotate(12 600 200)">PHANTOM</tspan>
@@ -221,6 +232,10 @@ export async function GET(
 
     if (format === 'png') {
       try {
+        // Инициализируем WASM перед использованием
+        await initializeResvg()
+        
+        // Render SVG -> PNG с WASM версией
         const resvg = new Resvg(svgContent, {
           fitTo: { mode: 'width', value: 800 },
           font: {
