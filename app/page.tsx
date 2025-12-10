9c11f9707fe732b3aa0c315ba30dec6989675f34
lucas555-ops <lucas9589716@gmail.com>
2025-12-10 18:24:30 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1043,7 +1043,7 @@ export default function Home() {
         </div>
        
         {/* Final Footer */}
-        <footer className="max-w-6xl mx-auto pt-8 border-t border-[#ab0ff2]/20">
+        <footer className="max-w-6xl mx-auto pt-8 pb-12 border-t border-[#ab0ff2]/20">
           <div className="flex flex-col md:flex-row justify-between items-center gap-6">
             <div className="flex items-center gap-3">
               <img 
@@ -1115,7 +1115,7 @@ export default function Home() {
                     <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
                   </svg>
                 </div>
-                <span>X / Twitter</span>
+                <span>X / Community</span>
               </a>
               
               <a 
